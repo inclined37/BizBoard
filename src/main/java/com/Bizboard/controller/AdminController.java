c5a306d7deb751e8bd0b740fdb58d24d06f96c1e
Kimjin <inclined37@gmail.com>
2023-06-28 17:33:17 +0900

    Add: Dept vo 추가, NoticeBoard 관련 mapper, Service, Controller, jsp 페이지 수정

--- src/main/java/com/Bizboard/controller/AdminController.java
+++ src/main/java/com/Bizboard/controller/AdminController.java
@@ -1,14 +1,20 @@
 package com.Bizboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.Bizboard.service.NoticeBoardService;
+import com.Bizboard.vo.Board;
 
 @Controller
 @RequestMapping("/admin/*")
 public class AdminController {
 
-	
+	@Autowired
+	private NoticeBoardService noticeBoardService; 
 	
 	@GetMapping("main")
 	public void main() {
@@ -17,7 +23,14 @@ public class AdminController {
 	
 	@GetMapping("noticeBoardInsert")
 	public void noticeBoard() {
-		
+		System.out.println("noticeBoardInsert 페이지 이동");
 	}
 	
+	@PostMapping("noticeBoardInsert")
+	public String noticeBoardInsert(Board board) {
+		System.out.println("noticeBoardInsert POST 요청 진입");
+		System.out.println(board.toString());
+		
+		return "redirect:/member/noticeBoard";
+	}
 }
---
+++ src/main/java/com/Bizboard/vo/Dept.java
@@ -0,0 +1,10 @@
+package com.Bizboard.vo;
+
+import lombok.Data;
+
+@Data
+public class Dept {
+	private int deptno;
+	private String dname;
+	private String loc;
+}
