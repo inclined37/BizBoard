e18ea211fbdc274d298a8c9538e42d6336ba021a
Kimjin <inclined37@gmail.com>
2023-06-29 14:01:39 +0900

    Add: vo BoardForm and BoardType

--- src/main/java/com/Bizboard/vo/BoardForm.java
+++ src/main/java/com/Bizboard/vo/BoardForm.java
@@ -1,8 +1,11 @@
 package com.Bizboard.vo;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 @Data
+@Component
 public class BoardForm {
 	private int bf_code;		//게시판 형식 코드
 	private String bf_name;		//게시판 형식 이름
--- src/main/java/com/Bizboard/vo/BoardType.java
+++ src/main/java/com/Bizboard/vo/BoardType.java
@@ -1,8 +1,11 @@
 package com.Bizboard.vo;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 @Data
+@Component
 public class BoardType {
 	private int bt_code;		//게시판 코드
 	private String bt_name;		//게시판 이름
